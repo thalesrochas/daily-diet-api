016fbda216b9d144d68ec1d43945902f47b12f6f
Thales Rocha <thales_rochasousa@hotmail.com>
2026-09-18 18:05:14 -0300

    Add Database

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import z from "zod";
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3000),
 });
 
